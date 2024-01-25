b427e49a66876a12f7aa4477e53e6edabe6d1a6b
lancenonce <lancendavis47@gmail.com>
2024-01-25 11:41:10 +0100

    initial comments

---
+++ examples/defi/lend.py
@@ -0,0 +1,11 @@
+# Imports
+
+# task get_function_abi_format(function_name: str)
+
+# task get_data()
+
+# task predict(input)
+
+# task generate_calldata(inference)
+
+# action execute_and_deploy(address, calldata)

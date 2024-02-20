ad0fea79b2e8f357cc6f95f9a16a072fa59c47cb
lancenonce <lancendavis47@gmail.com>
2024-02-20 12:20:31 +0100

    fix: contract fixes

--- examples/on-chain_mnist/contracts/src/MNISTNFT.sol
+++ examples/on-chain_mnist/contracts/src/MNISTNFT.sol
@@ -1,31 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
-contract MNISTNFT is ERC721, Ownable {
-
-    uint256 public mintPrice = 0.05 ether;
+contract MNISTNFT is ERC721 {
+    uint256 public mintPrice = 0.005 ether;
 
     mapping(uint256 => bool) public tokenExists;
 
-    constructor() ERC721("MNISTNFT", "NFT") {}
+    constructor() ERC721("MNISTNFT", "MNIST") {}
 
     function mint(uint256 tokenId) public payable {
         require(msg.value == mintPrice, "Incorrect mint price");
-        require(!tokenExists[tokenId], "Token already minted");
+        require(!tokenExists[tokenId], "Token already minted!");
 
         if (tokenExists[tokenId]) {
             tokenId = tokenId * 10;
         }
 
         tokenExists[tokenId] = true;
-        
+
         _safeMint(msg.sender, tokenId);
     }
-
-    function withdraw() public onlyOwner {
+    
+    // Not secure; anyone can withdraw
+    function withdraw() public {
         (bool success, ) = msg.sender.call{value: address(this).balance}("");
         require(success, "Transfer failed");
     }

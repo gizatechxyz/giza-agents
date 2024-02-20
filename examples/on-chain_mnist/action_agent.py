8fe66b8bf3953e11f00c58f9a03a2758db496d7b
lancenonce <lancendavis47@gmail.com>
2024-02-20 18:19:25 +0100

    process.env

--- examples/on-chain_mnist/action_agent.py
+++ examples/on-chain_mnist/action_agent.py
@@ -1,10 +1,20 @@
 import requests
+import numpy as np
+from PIL import Image
 from giza_actions.action import Action, action
-from giza_actions.agent import Agent
+from giza_actions.agent import GizaAgent
 from giza_actions.model import GizaModel
 from giza_actions.task import task
+from ape import accounts
+from ape import EIP712Message, EIP712Type
 
-input_size = 784 # 1x1x28x28
+class ProofType(EIP712Type):
+    proof_id: "string"
+    proof: "string"
+    address: "address"
+    
+class ProofMessage(EIP712Message):
+    proof: ProofType
 
 # Download model
 @task
@@ -21,16 +31,30 @@ def download_image():
     image_data = requests.get(image_url).content
     with open('seven.png', 'wb') as handler:
         handler.write(image_data)
-        
-# Create a GizaModel instance
 
-# Create GizaAgent instance
-
-# Task to load data
+# Process image
+@task
+def process_image(img):
+    img = img.convert('L')
+    img = img.resize((28,28))
+    img = np.array(img)
+    img = img.reshape(1,1,28,28)
+    img = img/255.0
+    print(img.shape)
+    return img
+    
+# Get image
+@task
+def get_image(path):
+    with Image.open(path) as img:
+        img = np.array(img.convert('RGB'))
+    return img
 
 # Run inference
 
 # Get the proof
+
+# Structure proof
 
 # Create an eip 712 message for signing the proof, then send off to the Agent to verify the signature
 
@@ -38,14 +62,43 @@ def download_image():
 
 # Define account details
 
+# Transmit 
+@task  
+def verify_and_transmit(agent: GizaAgent, account):
+    proof = agent._get_model_data()
+    
+    # Structure proof
+    signed_message = account.sign_message(message)
+    
+    # Send off to the Agent to verify the signature 
+    agent.verify(proof, signed_message)
+    
+    # Generate calldata 
+    calldata = agent.generate_calldata("contract.json", "setResult", [agent.inference])
+    
+    # Define account details
+    alias = "my_alias"
+    passphrase = "secret" 
+    mnemonic = "mnemonic phrase"
+    contract_address = "0x..."
+    
+    # Transmit transaction
+    receipt = agent.transmit(alias, passphrase, mnemonic, contract_address, "contract.json", model, proof, signed_message, calldata)
+    
+    return receipt
+
 # Create Action
 @action(log_prints=True)
 def execution():
+    img_path = 'seven.png'
+    img = get_image(img_path)
+    img = process_image(img)
+    model_path = 'mnist.onnx'
     download_model()
     download_image()
-    model = GizaModel(model_path='mnist.onnx')
-    agent = Agent(model)
-    agent.infer('seven.png')
+    model = GizaModel(model_path=model_path)
+    agent = GizaAgent(model)
+    agent.infer(img_path)
     agent.get_model_data()
     agent.verify()
     agent.deploy()

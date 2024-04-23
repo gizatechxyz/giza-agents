ccd9fd1491b013f65523159d8cf5577b9ec3411f
adrianvrj <adrivargas48@gmail.com>
2024-04-22 22:03:31 -0600

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,14 +5,13 @@ from giza_actions.utils import get_endpoint_uri
 
 endpoint_data = Endpoint(id=999, size="S", is_active=True, model_id=999, version_id=999, uri="testing.uri")
 endpoint_list = EndpointsList(root=[endpoint_data])
-
 @patch("giza.client.EndpointsClient.list", return_value=endpoint_list)
 def test_get_endpoint_uri_successful(mock_get):
     """
     Tests successful retrieval of the deployment URI for a model and version.
     """
     uri = get_endpoint_uri(model_id=788, version_id=23)
-    assert uri is not None
+    assert uri is "testing.uri"
     mock_get.assert_called_once()
 
 endpoint_list = EndpointsList(root=[])

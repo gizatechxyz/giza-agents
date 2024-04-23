df54f72f8a6f25a6b50f482b7a231ec4066af2e3
adrianvrj <adrivargas48@gmail.com>
2024-04-23 08:08:44 -0600

    added return value inside functions for readability

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,23 +3,25 @@ import pytest
 from giza.schemas.endpoints import Endpoint, EndpointsList
 from giza_actions.utils import get_endpoint_uri
 
-endpoint_data = Endpoint(id=999, size="S", is_active=True, model_id=999, version_id=999, uri="testing.uri")
-endpoint_list = EndpointsList(root=[endpoint_data])
-@patch("giza.client.EndpointsClient.list", return_value=endpoint_list)
+@patch("giza.client.EndpointsClient.list")
 def test_get_endpoint_uri_successful(mock_get):
     """
     Tests successful retrieval of the deployment URI for a model and version.
     """
+    endpoint_data = Endpoint(id=999, size="S", is_active=True, model_id=999, version_id=999, uri="testing.uri")
+    endpoint_list = EndpointsList(root=[endpoint_data])
+    mock_get.return_value = endpoint_list
     uri = get_endpoint_uri(model_id=788, version_id=23)
     assert uri is "testing.uri"
     mock_get.assert_called_once()
 
-endpoint_list = EndpointsList(root=[])
-@patch("giza.client.EndpointsClient.list", return_value=endpoint_list)
-def test_get_endpoint_uri_not_found(mock_get):
+@patch("giza.client.EndpointsClient.list")
+def test_get_endpoint_uri_not_found(mock_list):
     """
     Tests the case where no active deployment is found for the model and version.
     """
+    endpoint_list = EndpointsList(root=[])
+    mock_list.return_value = endpoint_list
     uri = get_endpoint_uri(model_id=516, version_id=19)
     assert uri is None
-    mock_get.assert_called_once()
+    mock_list.assert_called_once()

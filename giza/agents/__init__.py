8f3f7497b9b48b74dc3f66def84539ffd18a5239
Alejandro Martinez <alejandromartinez@Alejandros-MacBook-Pro.local>
2024-05-21 11:32:54 +0200

    fix: absolute paths in test agents

--- giza/agents/__init__.py
+++ giza/agents/__init__.py
@@ -1,10 +1,10 @@
 import pathlib
 
-from giza.agents.agent import AgentResult, ContractHandler, GizaAgent
+from giza.agents.agent import AgentResult, ContractHandler, GizaAgent, Contract
 
 # The absolute path to this module
 __module_path__ = pathlib.Path(__file__).parent
 # The absolute path to the root of the repository, only valid for use during development
 __development_base_path__ = __module_path__.parents[1]
 
-__all__ = [GizaAgent, AgentResult, ContractHandler]
+__all__ = ["GizaAgent", "AgentResult", "ContractHandler", "Contract"]
